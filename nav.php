93f9f17a4ab6a0578b32487e259721714fa6b4ed
Alex Gachassin <alexgachassin@gmail.com>
2022-10-24 14:23:18 -0500

    nav bar for pages

---
+++ nav.php
@@ -0,0 +1,27 @@
+<header>    
+	<!--navigation bar start-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg dark">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand"><img src="images\w-logo.png" width="auto" height="30px"></a>
+
+            <!--navbar toggle icon-->
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!--Collapsable Menu-->
+            <div class="collapse navbar-collapse " id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="index.home.php" class="nav-item nav-link">Wiki-Woo!</a>
+
+                </div>
+                <div class="navbar-nav ms-auto">         
+                    <a href="register.php" class="nav-item nav-link">Register User</a>  
+                    <a href="login.php" class="nav-item nav-link">Log In</a>   	
+                    <a href="logout.php" class="nav-item nav-link">Log Out</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!--navigation bar end-->
+</header>
