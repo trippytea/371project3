3bf70e339958a7f4c79d96fa102e4fb2e05406e1
Alex Gachassin <alexgachassin@gmail.com>
2022-10-24 14:09:13 -0500

    login with redirect to index.php

--- login.php
+++ login.php
@@ -1,1 +1,98 @@
+<?php 
+include('db.php'); //connect to database
 
+#if username and password are submitted, create variables
+if (isset($_POST["name"]) && isset($_POST["password"])) {
+    $name = $_POST["name"];
+    $password = $_POST["password"];
+    
+#call function in db.php to validate the password
+if (is_password_correct($name, $password, $db)) {
+    #if existing session, destroy old and start new
+    if (isset($_SESSION)) {
+      session_destroy();
+      session_regenerate_id(TRUE);
+      session_start();
+    }
+    # if user and pass match, remember user info and redirect to home
+    $_SESSION["name"] = $name;   
+    header("location: index.php");
+  }
+  
+  #else reload login page with error message
+  else {
+    header("location: login.php?err");
+  }
+}
+
+?> 
+<!--php ends-->
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+
+	<!--Open Sans Font-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+	rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> 
+	
+	<!-- Font Awesome icon library -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+ 	<!--<link rel="stylesheet" type="text/css" href="css\bootstrap.css"> if wanted offline-->
+
+	<!-- custom CSS Stylesheet -->	  
+    <link rel="stylesheet" type="text/css" href="styles.css";>
+</head>
+<body>
+    <div class="d-flex justify-content-center mx-auto">
+
+    <form method="post">
+    <h1 class="mt-5 mb-3 centerContent ">User Login</h1>
+    <?=$promptMessage()?> <!--call prompt message function-->            
+        <div class="form-outline mb-2">
+            <input type="text" name="name" id="name" class="form-control form-control-lg"/>
+            <label class="form-label" for="name">Username</label>
+        </div>
+
+        <div class="password-container form-outline mb-2">
+            <input type="password" name="password" id="password" class="form-control form-control-lg" />
+            <i class="fa-solid fa-eye" id="eye"></i>
+            <label class="form-label" for="password">Password</label>
+        </div>
+       
+            <button class="btn-primary btn-lg btn-block" type="submit" name='submit' value='Login'>Login</button>
+
+            <a href="index.php" style="text-decoration:none" class="mt-3">Home</a>
+            <span>&nbsp</span>
+            <a href="register.php" style="text-decoration:none" class="mt-3">Register User</a>
+        </form>
+       
+    </div>
+
+    <script> 
+        //select password field and eye icon(s)
+        const passwordField = document.querySelector("#password");
+        const eye= document.querySelector("#eye");
+        
+        eye.addEventListener("click", function() {
+        //toggle class from 'fa-solid fa-eye'(solid eye) to 'fa-eye-slash' (eye with slash)
+        this.classList.toggle("fa-eye-slash");
+
+        //set type constant as text if password, or password if text
+        const type = passwordField.getAttribute("type") === "password" ? "text" : "password";            
+        passwordField.setAttribute("type", type);
+    })
+    </script>
+
+	<!-- Bootstrap JS Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+<footer class="centerContent">Copyright &copy 2022 Wiki-Woo!</footer>
+</html>
