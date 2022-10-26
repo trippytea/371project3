29e2e82f3dd6e3ffc5903dcb20ed3fdd8226f2e0
Alex Gachassin <alexgachassin@gmail.com>
2022-10-25 19:20:59 -0500

    WIP addArticle

--- addarticle.php
+++ addarticle.php
@@ -1,28 +1,10 @@
-<!DOCTYPE html>
-<html lang="en-us">
-<html> 
-    <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <title>Create Article</title>
 
-    <!--Open Sans Font--> 
-    <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans' />
-
-    <!-- Bootstrap CSS -->
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' 
-    rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'> 
-
-    <!-- Font Awesome icon library -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'>
-
-    <!-- custom CSS Stylesheet -->	  
-    <link rel='stylesheet' type='text/css' href='styles.css';>
-</head>
 
 <!--php outputs body element-->
 <?php 
 include 'db.php';
+include 'nav.php'; //nav-bar
+
 ensure_logged_in();
 mysqli_report(MYSQLI_REPORT_STRICT);
 
@@ -57,36 +39,59 @@ if (isset($_POST['addArticleButton'])){
         $db, $_REQUEST['articleTitle']);
         $articleBody = mysqli_real_escape_string(
         $db, $_REQUEST['articleBody']);
+        $name = $_SESSION['name'];
         
-        $sql = "INSERT INTO team (articeTitle, articelBody, username)
-        VALUES ('$articleTitle','$articleBody','$_SESSION["name"]')";
+        $sql = "INSERT INTO article (articleTitle, articleBody, username)
+        VALUES ('$articleTitle','$articleBody','$name')";
 
         #ERROR MESSAGE
         # attempts the sql insert, if it fails the uniqueError is set
         if(mysqli_query($db, $sql)){
-            #header("location:create-teams.php?teamAdded");
+            header("location:addArticle.php?articleAdded");
             exit();
         } else {
             if(mysqli_errno($db) == 1062)
-            #header("location:create-teams.php?duplicateTeam");
+            header("location:.php?articleError");
             exit();
         }
     } 
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<html> 
+    <head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Create Article</title>
+
+    <!--Open Sans Font--> 
+    <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans' />
+
+    <!-- Bootstrap CSS -->
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' 
+    rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'> 
+
+    <!-- Font Awesome icon library -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'>
+
+    <!-- custom CSS Stylesheet -->	  
+    <link rel='stylesheet' type='text/css' href='styles.css';>
+</head>
 <body>
     <div style='margin-left: 20px;margin-top: 10px'>   
         <h1 style='margin-left: 10px;margin-top: 15px'>Add New Article</h1>
         <?=$promptMessage();?>
 
-        <form style='margin-left: 15px' id='createteams' action='create-teams.php' method='POST'>
+        <form style='margin-left: 15px' id='createArticle' action='addArticle.php' method='POST'>
             <span> Article Title </span><br>
             <input type='text' id='articleTitle' name='articleTitle'
             placeholder='Enter a title for the article' required>
             <br><br>
             <span> Article Body </span><br>
             <!-- i wonder if we could use a template for a text editor box -->
-            <textarea type='text' id='articleBody' name='articleBody'>
+            <textarea type='text' id='articleBody' name='articleBody'></textarea>
             <br><br>
             <input class='Add navbar-dark navbar-brand ' type='submit' id='addArticleButton' name='addArticleButton' value='Add'>
         </form>
@@ -96,7 +101,5 @@ if (isset($_POST['addArticleButton'])){
     <!-- Bootstrap JS Bundle with Popper ***needed for navbar collapse*** -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
-    <footer class="centerContent">simpleWIKI</footer> -->
+    <footer class="centerContent">Copyright &copy 2022 Wiki-Woo!</footer>
 </html>
-
- 
