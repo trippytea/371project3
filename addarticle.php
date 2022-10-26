4737100a1283153eb57944d53785aa63a454290d
Alex Gachassin <alexgachassin@gmail.com>
2022-10-25 22:21:30 -0500

    increased textarea size

--- addarticle.php
+++ addarticle.php
@@ -95,7 +95,7 @@ if (isset($_POST['addArticleButton'])){
             <br><br>
             <span> Article Body </span><br>
             <!-- i wonder if we could use a template for a text editor box -->
-            <textarea type='text' id='articleBody' name='articleBody'></textarea>
+            <textarea type='text' id='articleBody' name='articleBody' rows="10" cols="50"></textarea>
             <br><br>
             <input class='Add navbar-dark navbar-brand ' type='submit' id='addArticleButton' name='addArticleButton' value='Add'>
         </form>
